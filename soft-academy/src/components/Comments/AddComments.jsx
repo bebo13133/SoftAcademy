ff17f6a7c4ef911f2e59d8e7b7e8d0f79ed262df
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-10-30 20:13:58 +0200

    Update AddComments.jsx

--- soft-academy/src/components/Comments/AddComments.jsx
+++ soft-academy/src/components/Comments/AddComments.jsx
@@ -1,23 +1,23 @@
 import { useForm } from "../Hooks/useForm"
 import { RouteGuard } from "../common/RouteGuard"
 
-export const AddComments=({
+export const AddComments = ({
     onCommentSubmit
 
-})=>{
-    const {values,onChangeHandler,onSubmit}=useForm({
-        comment:"",
-    },onCommentSubmit)
-    return(
-    <RouteGuard>
-        <article className="create-comment">
-        <label>Add new comment:</label>
-        <form className="form-comment" onSubmit={onSubmit}>
-            {/* <input type="text" name="username" placeholder="name" value={values.username} onChange={onChangeHandler} ></input> */}
-            <textarea name="comment" placeholder="Comment......" value={values.comment} onChange={onChangeHandler}></textarea>
-            <input className="btn-comment submit" type="submit" value="Add Comment" />
-        </form>
-    </article>
-    </RouteGuard>
+}) => {
+    const { values, onChangeHandler, onSubmit } = useForm({
+        comment: "",
+    }, onCommentSubmit)
+    return (
+        <RouteGuard>
+            <article className="create-comment">
+                <label>Add new comment:</label>
+                <form className="form-comment" onSubmit={onSubmit}>
+                    {/* <input type="text" name="username" placeholder="name" value={values.username} onChange={onChangeHandler} ></input> */}
+                    <textarea name="comment" placeholder="Comment......" value={values.comment} onChange={onChangeHandler}></textarea>
+                    <input className="btn-comment submit" type="submit" value="Add Comment" />
+                </form>
+            </article>
+        </RouteGuard>
     )
 }
