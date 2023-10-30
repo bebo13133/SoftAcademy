1b3d0da7acc5bffe8a66ce911bea9740c0115c7b
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-10-30 20:15:36 +0200

    Update AddComments.jsx

--- soft-academy/src/components/Comments/AddComments.jsx
+++ soft-academy/src/components/Comments/AddComments.jsx
@@ -13,7 +13,6 @@ export const AddComments = ({
             <article className="create-comment">
                 <label>Add new comment:</label>
                 <form className="form-comment" onSubmit={onSubmit}>
-                    {/* <input type="text" name="username" placeholder="name" value={values.username} onChange={onChangeHandler} ></input> */}
                     <textarea name="comment" placeholder="Comment......" value={values.comment} onChange={onChangeHandler}></textarea>
                     <input className="btn-comment submit" type="submit" value="Add Comment" />
                 </form>
