a2f8fba2374ff2ce529ef922ff85addc047000ec
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-11-10 20:29:51 +0200

    Update CourseContext.jsx

--- soft-academy/src/components/contexts/CourseContext.jsx
+++ soft-academy/src/components/contexts/CourseContext.jsx
@@ -1,6 +1,6 @@
 import { courseServiceFactory } from "../Services/courseService";
 import { createContext, useContext, useEffect, useState } from "react";
-import {useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useAuthContext } from "./UserContext";
 
 
@@ -8,7 +8,7 @@ export const CourseContext = createContext()
 
 
 export const CourseProvider = ({ children }) => {
-const{token}=useAuthContext()
+    const { token } = useAuthContext()
     const [course, setCourse] = useState([])
     const [searchResult, setSearchResult] = useState([])
 
@@ -26,19 +26,19 @@ const{token}=useAuthContext()
 
     const onCreateCourseSubmit = async (courseData) => {
         try {
-            if(!courseData.courseName ||
-            !courseData.firstName||
-            !courseData.lastName||
-            !courseData.email||
-            !courseData.ownerCourse||
-            !courseData.price||
-            !courseData.description||
-            !courseData.lectorDescription) return alert("Some field is empty")
+            if (!courseData.courseName ||
+                !courseData.firstName ||
+                !courseData.lastName ||
+                !courseData.email ||
+                !courseData.ownerCourse ||
+                !courseData.price ||
+                !courseData.description ||
+                !courseData.lectorDescription) return alert("Some field is empty")
 
-            if(courseData.courseName.length<4 || courseData.firstName.length<4 
-            || courseData.lastName.length<4 || courseData.email.length<9) return alert("Minimum field length is 4 for names and 9 for email")
+            if (courseData.courseName.length < 4 || courseData.firstName.length < 4
+                || courseData.lastName.length < 4 || courseData.email.length < 9) return alert("Minimum field length is 4 for names and 9 for email")
 
-            if(courseData.lectorDescription.length<5 || courseData.description.length<5 ) return alert("Minimum field description length is 5")
+            if (courseData.lectorDescription.length < 5 || courseData.description.length < 5) return alert("Minimum field description length is 5")
 
             const newCourse = await courseService.create(courseData)
 
@@ -79,7 +79,7 @@ const{token}=useAuthContext()
             setCourse(courses => courses.map(x => x._id === data._id ? result : x))
             navigate(`/catalog/${data._id}`)
         } catch (err) {
-           
+
         }
     }
 
@@ -100,20 +100,20 @@ const{token}=useAuthContext()
                     const searchResults = result.filter(course =>
                         searchTerms.every(term => course.courseName?.toLowerCase().includes(term))
                     );
-                         setSearchResult(searchResults);
+                    setSearchResult(searchResults);
                 } else if (data.criteria == 'language-name') {
                     setSearchResult(result.filter(course => course.selectOption?.toLowerCase().includes(data.searchName.toLowerCase())));
-                } else if (data.criteria =='lector-name') {
+                } else if (data.criteria == 'lector-name') {
                     setSearchResult(result.filter(course =>
                         course.firstName?.toUpperCase().includes(data.searchName.toUpperCase())
-                         || course.lastName?.toLowerCase().includes(data.searchName.toLowerCase())));
-          
+                        || course.lastName?.toLowerCase().includes(data.searchName.toLowerCase())));
+
 
                 }
 
             }
-    // console.log("result",result)
-           
+            // console.log("result",result)
+
             if (searchResult.length > 0) {
                 navigate("/search-page");
             }
