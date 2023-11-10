8688ad16cf5750ddb93fcf4fdcc3f6b2268d3ec6
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-11-10 20:29:24 +0200

    Update forumService.jsx

--- soft-academy/src/components/Services/forumService.jsx
+++ soft-academy/src/components/Services/forumService.jsx
@@ -2,28 +2,28 @@ import { requestFactory } from "./requester"
 const baseUrl = `http://localhost:3030/data/forums`
 
 
-export const forumServiceFactory=(token)=>{
+export const forumServiceFactory = (token) => {
 
-const request = requestFactory(token)
+    const request = requestFactory(token)
 
-const getAll = async () => {
-    const result = await request.get(baseUrl)
+    const getAll = async () => {
+        const result = await request.get(baseUrl)
 
-    const posts = Object.values(result)
-    return posts
-}
+        const posts = Object.values(result)
+        return posts
+    }
 
 
-const create = async (forumData) => {
+    const create = async (forumData) => {
 
-    const result = await request.post(baseUrl, forumData)
-    return result
-}
-return {
-     getAll,
-    create,
-    // getOne,
-    // delete: deleteCourse,
-    // update: updateCourse,
-}
+        const result = await request.post(baseUrl, forumData)
+        return result
+    }
+    return {
+        getAll,
+        create,
+        // getOne,
+        // delete: deleteCourse,
+        // update: updateCourse,
+    }
 }
