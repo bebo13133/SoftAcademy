72d6a720073873966392b58ec646219912e94818
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-11-10 19:11:23 +0200

    add

--- soft-academy/src/components/Forum/AddForumPost/AddForumPost.jsx
+++ soft-academy/src/components/Forum/AddForumPost/AddForumPost.jsx
@@ -1,21 +1,50 @@
+import { useForm } from "../../Hooks/useForm"
+import { useState } from "react";
+import './addForumPost.css'
+
+
 export const AddForumPost = () => {
+    const [image, setImage] = useState(null);
+
+
+    const { onSubmit, values, onChangeHandler } = useForm({
+        title:"",
+description:"",
+image:"",
+author:"",
+createdAt:"",
+    })
+
+    const handleImageChange = (e) => {
+        const file = e.target.files[0];
+        setImage(file);
+      };
+
 
     return (
 
-
         <>
-            <form onSubmit={handleSubmit}>
+            <form className="post-form" onSubmit={onSubmit}>
                 <label>
                     Title:
-                    <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+                    <input type="text" name="title" value={values.title} onChange={onChangeHandler} />
                 </label>
                 <label>
-                    Content:
-                    <textarea value={content} onChange={(e) => setContent(e.target.value)} />
+                   Description: 
+                    <textarea  name="description" value={values.description} onChange={onChangeHandler} />
                 </label>
                 <label>
                     Image:
-                    <input type="file" onChange={handleImageChange} accept="image/*" />
+                    <input type="file" ame="image" onChange={handleImageChange} accept="image/*" />
+                </label>
+
+                <label>
+                    Author:
+                    <input type="text" name="author" value={values.author} onChange={onChangeHandler} />
+                </label>
+                <label>
+                    Created At:
+                    <input type="text" name="createdAt" value={new Date().toLocaleString()} readOnly />
                 </label>
                 {image && (
                     <div>
@@ -23,6 +52,7 @@ export const AddForumPost = () => {
                         <img src={URL.createObjectURL(image)} alt="Preview" style={{ maxWidth: '100%', maxHeight: '200px' }} />
                     </div>
                 )}
+
                 <button type="submit">Create Post</button>
             </form>
 
