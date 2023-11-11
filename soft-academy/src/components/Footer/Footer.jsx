ed6107a0136a5ccd29d1644a60d1b52b258ad361
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-11-11 19:10:49 +0200

    Update Footer.jsx

--- soft-academy/src/components/Footer/Footer.jsx
+++ soft-academy/src/components/Footer/Footer.jsx
@@ -5,48 +5,48 @@ export const Footer = () => {
     return (
         <>
             <footer className="footer">
-      {/* Section 1: All Rights Reserved */}
-      <section className="footer-section rights-reserved">
-        <h3>All Rights Reserved</h3>
-        <ul><li className="navbar-brand " style={{ fontSize: "16px", fontWeight: "bold", color: "#ff545a" }} href="/">Soft<span style={{ fontSize: "16px", textTransform: "none", color: "black" }}>Academy</span></li></ul>
-        <p>&copy; 2023  </p> 
-      </section>
+                {/* Section 1: All Rights Reserved */}
+                <section className="footer-section rights-reserved">
+                    <h3>All Rights Reserved</h3>
+                    <ul><li className="navbar-brand " style={{ fontSize: "16px", fontWeight: "bold", color: "#ff545a" }} href="/">Soft<span style={{ fontSize: "16px", textTransform: "none", color: "black" }}>Academy</span></li></ul>
+                    <p>&copy; 2023  </p>
+                </section>
 
-      {/* Section 2: Address and Phone Column */}
-      <section className="footer-section contact">
-        <h3>Contact</h3>
-        <p>Address: str.Krusha Planina 56a, Sofia, Bulagria</p>
-        <p>Phone: +359 894 371 779</p>
-      </section>
+                {/* Section 2: Address and Phone Column */}
+                <section className="footer-section contact">
+                    <h3>Contact</h3>
+                    <p>Address: str.Krusha Planina 56a, Sofia, Bulagria</p>
+                    <p>Phone: +359 894 371 779</p>
+                </section>
 
-      {/* Section 3: Terms */}
-      <section className="footer-section terms">
-        <h3>Terms</h3>
-        <ul>
-          <li><a href="/terms">Terms and Conditions</a></li>
-          <li><a href="/privacy">Privacy Policy</a></li>
-        </ul>
-      </section>
+                {/* Section 3: Terms */}
+                <section className="footer-section terms">
+                    <h3>Terms</h3>
+                    <ul>
+                        <li><a href="/terms">Terms and Conditions</a></li>
+                        <li><a href="/privacy">Privacy Policy</a></li>
+                    </ul>
+                </section>
 
-      {/* Section 4: Social Media */}
-      <section className="footer-section social-media">
-        <h3>Social Media</h3>
-        <div className="social-icons">
-          <a href="https://www.facebook.com/groups/706263354783010"><img src="/img/Facebook_Logo_2023.png" alt="Facebook" /></a>
-          <a href="https://twitter.com"><img src="/img/twiter-png.webp" alt="Twitter" /></a>
-          <a href="https://instagram.com"><img src="/img/insagram.png" alt="Instagram" /></a>
-        </div>
-      </section>
-    </footer>
+                {/* Section 4: Social Media */}
+                <section className="footer-section social-media">
+                    <h3>Social Media</h3>
+                    <div className="social-icons">
+                        <a href="https://www.facebook.com/groups/706263354783010"><img src="/img/Facebook_Logo_2023.png" alt="Facebook" /></a>
+                        <a href="https://twitter.com"><img src="/img/twiter-png.webp" alt="Twitter" /></a>
+                        <a href="https://instagram.com"><img src="/img/insagram.png" alt="Instagram" /></a>
+                    </div>
+                </section>
+            </footer>
 
-                {/* <div id="scroll-Top">
+            {/* <div id="scroll-Top">
                     <div className="return-to-top">
                         <i className="fa fa-angle-up " id="scroll-top" data-toggle="tooltip" data-placement="top" title="" data-original-title="Back to Top" aria-hidden="true"></i>
                     </div>
 
                 </div> */}
 
-          
+
         </>
 
     )
