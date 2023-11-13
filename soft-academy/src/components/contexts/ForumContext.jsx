5952dee857d27d23edc6aeff0794f24c3705bcbb
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-11-13 09:20:42 +0200

    add

--- soft-academy/src/components/contexts/ForumContext.jsx
+++ soft-academy/src/components/contexts/ForumContext.jsx
@@ -3,13 +3,14 @@ import { createContext, useEffect, useState, useContext } from "react";
 import { forumServiceFactory } from "../Services/forumService";
 import { useAuthContext } from "./UserContext";
 import { useNavigate } from "react-router-dom";
-
+import './error.css';
 const forumContext = createContext()
 
 
 export const ForumProvider = ({ children }) => {
 
     const [forumPosts, setForumPosts] = useState([])
+    const [errorMessage, setErrorMessage] = useState(''); //error messages
     const { token } = useAuthContext()
     const forumService = forumServiceFactory(token)
     const navigate = useNavigate()
@@ -27,17 +28,29 @@ export const ForumProvider = ({ children }) => {
 
 
         try {
-            // if(!courseData.courseName ||
-            // !courseData.firstName||
-            // !courseData.lastName||
-            // !courseData.email||
-            // !courseData.ownerCourse||
-            // !courseData.price||
-            // !courseData.description||
-            // !courseData.lectorDescription) return alert("Some field is empty")
 
-            // if(courseData.courseName.length<4 || courseData.firstName.length<4 
-            // || courseData.lastName.length<4 || courseData.email.length<9) return alert("Minimum field length is 4 for names and 9 for email")
+            if (!forumData.title || !forumData.description || !forumData.author || !forumData.imageUrl) { 
+                setErrorMessage("Some fields is empty")
+            
+                setTimeout(() => {
+                    setErrorMessage('');
+                  }, 4000);
+            
+                  return;
+            }
+
+
+            if (forumData.author.length < 4
+                || forumData.description.length < 4
+                || forumData.title.length < 4){
+
+                     setErrorMessage("Minimum field length is 4 for names")
+                    setTimeout(() => {
+                        setErrorMessage('');
+                      }, 4000);
+                
+                      return;
+                }
 
             // if(courseData.lectorDescription.length<5 || courseData.description.length<5 ) return alert("Minimum field description length is 5")
 
@@ -68,16 +81,39 @@ export const ForumProvider = ({ children }) => {
     }
 
     const onEditSubmitPost = async (forumData) => {
-       
 
-            try {
-                 if (!forumData.title || !forumData.description|| !forumData.author || !forumData.imageUrl) return alert("Some fields is empty")
-                const result = await forumService.update(forumData._id, forumData)
-                setForumPosts(state => state.map(x => x._id === forumData._id ? result : x))
-                navigate(`/forum/${forumData._id}`)
-            } catch (err) {
 
+        try {
+            if (!forumData.title || !forumData.description || !forumData.author || !forumData.imageUrl){
+                setErrorMessage("Some fields is empty")
+                setErrorMessage("Minimum field length is 4")
+                setTimeout(() => {
+                    setErrorMessage('');
+                  }, 4000);
+            
+                  return;
             }
+            
+   
+
+            if (forumData.author.length < 4
+                || forumData.description.length < 4
+                || forumData.title.length < 4){
+
+                    setErrorMessage("Minimum field length is 4")
+                    setTimeout(() => {
+                        setErrorMessage('');
+                      }, 4000);
+                
+                      return;
+                }
+
+            const result = await forumService.update(forumData._id, forumData)
+            setForumPosts(state => state.map(x => x._id === forumData._id ? result : x))
+            navigate(`/forum/${forumData._id}`)
+        } catch (err) {
+
+        }
     }
 
 
@@ -94,13 +130,18 @@ export const ForumProvider = ({ children }) => {
         <forumContext.Provider value={contextForumValue}>
 
             {children}
-
+    {errorMessage && (
+        <div className={`error-message ${errorMessage && 'show-error custom-style'}`}>
+          <p>{errorMessage}</p>
+        </div>
+      )}
         </forumContext.Provider>
 
 
     )
 
 }
+
 export const useForumContext = () => {
     const context = useContext(forumContext);
 
