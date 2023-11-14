547e180601faae2ed16519750743ad8b3cefb50a
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-11-14 08:38:24 +0200

    add

--- soft-academy/src/components/Home/LastForumsCarousel/LastForumsCarousel.jsx
+++ soft-academy/src/components/Home/LastForumsCarousel/LastForumsCarousel.jsx
@@ -1,5 +1,7 @@
 import Slider from "react-slick";
 
+import './LastForumsCarousel.css'
+import { OneSliderComponent } from "./OneSliderComponent";
 export const LastForumsCarousel = () => {
 
     const settings = {
@@ -9,30 +11,25 @@ export const LastForumsCarousel = () => {
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
-        initialSlide: 2,
+        // initialSlide: 2,
 
-      };
+    };
+
+    
 
     return (
 
         <>
-            <div>
-                <h2>Fade</h2>
-                <Slider {...settings}>
-                    <div>
-                        <img src="https://capitalizemytitle.com/wp-content/uploads/2020/08/Has-vs.-Have.png" />
-                    </div>
-                    <div>
-                        <img src="https://capitalizemytitle.com/wp-content/uploads/2020/08/Has-vs.-Have.png" />
-                    </div>
-                    <div>
-                        <img src={"baseUrl" + "/abstract03.jpg"} />
-                    </div>
-                    <div>
-                        <img src={"baseUrl" + "/abstract04.jpg"} />
-                    </div>
-                </Slider>
-            </div>
+            <div className="blue-bar">
+                <div>
+                    <h2>Fade</h2>
+                    <Slider {...settings}>
+                     
+                       <OneSliderComponent/>
+                      
+                    </Slider>
+                </div>
+            </div>;
         </>
 
     )
---
+++ soft-academy/src/components/Home/LastForumsCarousel/OneSliderComponent.jsx
@@ -0,0 +1,14 @@
+export const OneSliderComponent = () => {
+    return (
+        <>
+            <div className="my-component-container">
+                <img src="https://capitalizemytitle.com/wp-content/uploads/2020/08/Has-vs.-Have.png" alt="Example" className="my-component-image" />
+                <div className="my-component-content">
+                    <h2 className="my-component-title">Заглавие</h2>
+                    <p className="my-component-description">Описание на компонента...</p>
+                </div>
+            </div>
+        </>
+
+    )
+}
