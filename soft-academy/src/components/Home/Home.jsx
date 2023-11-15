1a338f8989192279df62331f732cb482076593d8
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-11-15 19:26:57 +0200

    add

--- soft-academy/src/components/Home/Home.jsx
+++ soft-academy/src/components/Home/Home.jsx
@@ -59,19 +59,19 @@ export const Home = () => {
                         <h2>Welcome  in software world  <br />and best  software engineering courses </h2>
                         </Slide>
                         <Slide direction="left" duration="2500" triggerOnce='true'>
-                        <p>
+                        <p >
                             Find Best Place, Restaurant, Hotel, Real State and many more think in just One click
                         </p>
                         </Slide>
 
                         <Slide direction="right" duration="2500" triggerOnce='true'>
-                        <p><i class="fa fa-check" aria-hidden="true"></i>&nbsp;Comprehensive Software program</p>
+                        <p style={{color:"#f5a754"}}><i style={{color:"#f5a754"}}class="fa fa-check" aria-hidden="true"></i>&nbsp;Comprehensive Software program</p>
                         </Slide>
                         <Slide direction="left" duration="2500" triggerOnce='true'>
-                        <p><i class="fa fa-check" aria-hidden="true"></i>&nbsp;Learn how build DIY apps yourself&nbsp;&nbsp;</p>
+                        <p style={{color:"#f5a754"}}><i style={{color:"#f5a754"}} class="fa fa-check" aria-hidden="true"></i>&nbsp;Learn how build DIY apps yourself&nbsp;&nbsp;</p>
                         </Slide>
                         <Slide direction="right" duration="2500" triggerOnce='true'>
-                        <p><i class="fa fa-check" aria-hidden="true"></i>&nbsp;Become a successful software engineer</p>
+                        <p style={{color:"#f5a754"}}><i style={{color:"#f5a754"}} class="fa fa-check" aria-hidden="true"></i>&nbsp;Become a successful software engineer</p>
                         </Slide>
                     </div>
                     
