9bb687e3a9e17a51a0fd2de19851d436207febde
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-11-18 15:08:30 +0200

    add

--- soft-academy/src/components/ResponsiveUserBar.jsx
+++ soft-academy/src/components/ResponsiveUserBar.jsx
@@ -1,21 +1,11 @@
 import { useNavigate } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { ref, uploadBytes, listAll, getDownloadURL } from "firebase/storage"
+import './responsiveUserBar.css'
 
 
 
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
 
-import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
-
-import Tooltip from "@mui/material/Tooltip";
-import MenuItem from "@mui/material/MenuItem";
 import { AvatarChange } from "./ProfilePage/AvatarChange";
 import { storage } from "./config/firebase-config";
 import { useAuthContext } from "./contexts/UserContext";
@@ -28,14 +18,15 @@ const settings = ["Profile", "Settings", "Logout"];
 
 export const ResponsiveUserBar = () => {
   const navigate = useNavigate();
-  const [anchorElUser, setAnchorElUser] = useState(null);
+  const [anchorElUser, setAnchorElUser] = useState(false);
+  console.log("anchorElUser",anchorElUser)
   const [avatars, setAvatar] = useState([]);
   const [urlAvatar, setUrlAvatar] = useState([]);
 const [isLoading,setIsLoading] = useState(true);
   const { userId, avatarUrl} = useAuthContext()
 // console.log("avatar",avatarUrl)
   const avatarImageList = ref(storage, `avatarImages/`)
-
+  const menuRef = useRef(null);
 
  const fetchUserLastAvatar = (userId) => {
     listAll(avatarImageList)
@@ -70,13 +61,24 @@ setIsLoading(false)
 
 
 
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
+  // const handleOpenUserMenu = (event) => {
+  //   setAnchorElUser(true);
+  // };
+
+  // const handleCloseUserMenu = () => {
+  //   setAnchorElUser(false);
+  // };
+  // const handleAvatarClick = () => {
+  //   console.log("hi")
+  //   handleCloseUserMenu();
+  // };
+
+  const handleToggleUserMenu = (event) => {
+    setAnchorElUser((prevAnchor) =>
+      prevAnchor ? false : true
+    );
   };
 
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
 
   const handleLogout = () => {
     navigate('/logout', { replace: true });
@@ -90,56 +92,47 @@ setIsLoading(false)
 
 
 
-
-
   // export { fetchUserLastAvatar };
-
+  // useEffect(() => {
+  //   if (!anchorElUser) {
+  //     handleCloseUserMenu();
+  //   }
+  // }, [anchorElUser]);
   return (
     <>
-    {isLoading && <IsLoading/>}
-    <AppBar position="static" style={{ boxShadow: 'none', backgroundColor: '#f1f1f1',}}>
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-
-
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-
-          
-                <Avatar alt="Remy Sharp" src={avatarUrl || urlAv[1] || "/img/avatar-user.png"} style={{ width: 50, height: 50 }}/>
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: "45px" }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right"
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right"
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={setting === 'Profile' ? handleProfile :
-                  setting === 'Settings' ? handleSettings :
-                    setting === 'Logout' ? handleLogout :
-                      handleCloseUserMenu}>
-                  <Typography textAlign="center" style={{fontSize:"1.4rem"}}>{setting}</Typography>
-                </MenuItem>
-              ))}
-
-            </Menu>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+    <div className="user-bar-container">
+        <button
+          onClick={handleToggleUserMenu}
+          className="user-avatar-button"
+        >
+          <img
+            alt="User Avatar"
+            src={avatarUrl || urlAv[1] || "/img/avatar-user.png"}
+            className="user-avatar"
+          />
+        </button>
+        {Boolean(anchorElUser)&& (
+          <div className="user-menu">
+            {settings.map((setting) => (
+              <button
+                key={setting}
+                onClick={() =>
+                  setting === "Profile"
+                    ? handleProfile()
+                    : setting === "Settings"
+                    ? handleSettings()
+                    : setting === "Logout"
+                    ? handleLogout()
+                    : handleCloseUserMenu()
+                }
+                className="menu-item"
+              >
+                {setting}
+              </button>
+            ))}
+          </div>
+        )}
+      </div>
     </>
   );
 }
