e897ff1f5d18644d951c1386ab59ed2cfc7b47b6
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-11-21 12:18:48 +0200

    add

--- soft-academy/src/components/AdminDashboard/AllForums/AllForums.jsx
+++ soft-academy/src/components/AdminDashboard/AllForums/AllForums.jsx
@@ -1,8 +1,35 @@
-export const AllForums = ()=>{
-    return(
+export const AllForums = () => {
+
+
+
+    return (
 
 
         <>
+            <div className="admin-dashboard">
+
+                {/* <section className="sidebar">
+    <AdminSidebar />
+</section> */}
+
+
+                <section className="render-section">
+
+                    <div className="customer-list">
+                        <h2>All courses</h2>
+                        <SearchBarAdminCourses />
+                        {currentResults && currentResults.map(user => <RowSectionForums key={user._id} onDeleteClick={() => handleDelete(user._id)} {...user} />)}
+                    </div>
+                    <ul className="pagination-admin">
+                        {Array.from({ length: totalPages }, (_, index) => (
+                            <li key={index} onClick={() => setCurrentPage(index + 1)} className={currentPage === index + 1 ? "active" : ""}>
+                                {index + 1}
+                            </li>
+                        ))}
+                    </ul>
+                </section>
+
+            </div>
         </>
     )
 }
