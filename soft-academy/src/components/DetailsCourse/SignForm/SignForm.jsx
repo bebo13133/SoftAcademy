28e47b532ad0cede58a4364aa4b17974033a10ec
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-11-24 09:07:08 +0200

    add

--- soft-academy/src/components/DetailsCourse/SignForm/SignForm.jsx
+++ soft-academy/src/components/DetailsCourse/SignForm/SignForm.jsx
@@ -1,5 +1,5 @@
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Footer from "../../Footer/Footer"
 import "./signForm.css"
 import { useForm } from "../../Hooks/useForm"
@@ -7,27 +7,42 @@ import { useParams } from "react-router-dom"
 import { useCourseContext } from "../../contexts/CourseContext"
 import { useAuthContext } from "../../contexts/UserContext"
 import { FaCheck } from 'react-icons/fa';
+import { courseServiceFactory } from "../../Services/courseService"
 
 export const SignForm = () => {
+    const [course,setCourse]=useState([])
+    const { userId,token } = useAuthContext()
+    const courseService = courseServiceFactory(token)
     const [isChecked, setIsChecked] = useState(false)
-    console.log(isChecked)
-    const { userId } = useAuthContext()
+    // console.log(isChecked)
+  
 
     const courseIdObject = useParams()
     // console.log(courseId.courseId,"courseId)courseId)courseId)")
-const courseId =courseIdObject.courseId 
+    const courseId = courseIdObject.courseId
     const owner = userId
     const { onSignUp } = useCourseContext()
     const handleTermsChange = () => {
         setIsChecked(state => !state);
     };
 
+    useEffect(()=>{
+        courseService.getOne(courseId)
+        .then(result=>{
+            setCourse(result)
+        })
+        .catch(error => {
+            console.log(error.message||error)
+        })
+    },[courseId,owner,userId])
+
+
     const { onSubmit, onChangeHandler, values } = useForm({
         firstName: "",
         lastName: "",
         phoneNumber: "",
         socialNumber: "",
-       courseId,
+        courseId,
         isChecked,
         owner
 
@@ -40,7 +55,7 @@ const courseId =courseIdObject.courseId
             <div className="form-container-outer">
                 <section className="form-container-sign">
                     <form className="user-form-sign" onSubmit={onSubmit}>
-                        <h3>Sign up for a course -</h3>
+                        <h3>Sign up for a course - <strong>{course.courseName}</strong></h3>
                         <div className="divider"></div>
 
                         <div className="name-inputs-sign">
@@ -48,7 +63,7 @@ const courseId =courseIdObject.courseId
                             <input type="text" name="lastName" className="last-name" value={values.lastName} placeholder="Last Name" onChange={onChangeHandler} />
                         </div>
                         <input type="tel" name="phoneNumber" className="phone-number" value={values.phoneNumber} placeholder="Phone Number" onChange={onChangeHandler} />
-                        <input type="text" name="socialNumber" className="dna-number" value={values.socialNumber} placeholder="Social number" onChange={onChangeHandler} />
+                        <input type="number" name="socialNumber" className="dna-number" value={values.socialNumber} placeholder="Social number" onChange={onChangeHandler} />
                         <label >
                             <input className="checkbox-sign"
                                 name="checkbox-sign"
@@ -68,16 +83,16 @@ const courseId =courseIdObject.courseId
                 </section>
                 <section className="sign-course-card-finish">
                     <img className="sign-course-img" src="/img/joinUp.webp" alt="sign-course" />
-                    <h2>Course: <span>""</span> </h2>
-                    <h3> Price ""</h3>
+                    <h2>Course: <span>{course.courseName}</span> </h2>
+                    <h3> Price: {course.price}</h3>
                     <span></span>
-                  <p className="lector-disc">  <FaCheck color="red"/> Start in: ""
+                    <p className="lector-disc">  <FaCheck color="red" /> Start in: 
                     </p>
-                    <h4 className="lector-disc-h4">19.10.2023</h4>
-                   
-                    <p className="lector-disc"> <FaCheck color="red"/>  Duration of the course</p>
+                    <h4 className="lector-disc-h4">{course.date}</h4>
 
-                    <h4 className="lector-disc-h4"> "" weeks </h4>
+                    <p className="lector-disc"> <FaCheck color="red" />  Duration of the course</p>
+
+                    <h4 className="lector-disc-h4"> {course.weeksCourse} weeks </h4>
 
                 </section>
 
