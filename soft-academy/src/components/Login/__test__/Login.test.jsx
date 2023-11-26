0453f4f7f7aa63e160daee3f1aa7f06218469481
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-11-26 09:13:45 +0200

    add tests

--- soft-academy/src/components/Login/__test__/Login.test.jsx
+++ soft-academy/src/components/Login/__test__/Login.test.jsx
@@ -1,120 +1,166 @@
 // import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-import { Login } from '../Login'; 
+import { Login } from '../Login';
 import { UserProvider } from '../../contexts/UserContext';
 import { BrowserRouter, Router } from 'react-router-dom';
 import 'intersection-observer';
-test('renders login form correctly', () => {
-    render(
-        <BrowserRouter>
-            <UserProvider>
-                <Login />
-            </UserProvider>
-        </BrowserRouter>
-    );
+import { describe } from 'vitest';
+describe('Login component', () => {
+    let renderComponent
+    let emailInput;
+    let passwordInput;
+    beforeEach(() => {
+        renderComponent = render(
+            <BrowserRouter>
+                <UserProvider>
+                    <Login />
+                </UserProvider>
+            </BrowserRouter>
+        )
+
+        emailInput = screen.getByLabelText('Email:');
+        passwordInput = screen.getByLabelText('Password:');
+
+    })
+    it('renders login form correctly', () => {
+        // const emailInput = screen.getByLabelText('Email:');
+        // const passwordInput = screen.getByLabelText('Password:');
+
+        expect(screen.getByText("Login")).toBeInTheDocument();
+        expect(emailInput).toBeInTheDocument();
+        expect(passwordInput).toBeInTheDocument();
 
 
-    expect(screen.getByText("IF you don't have a PROFILE CLICK")).toBeInTheDocument();
+        fireEvent.change(emailInput, { target: { value: 'test@example.com' } });
+        fireEvent.change(passwordInput, { target: { value: 'testPassword' } });
+
+        expect(emailInput.value).toBe('test@example.com');
+        expect(passwordInput.value).toBe('testPassword');
+        const submitButton = screen.getByText('Login');
+        fireEvent.click(submitButton);
+
+    });
+
+    it('successful login redirects to home', async () => {
+
+        // emailInput = screen.getByLabelText(/email/i);
+        //  emailInput = screen.getByLabelText("Password:")
+
+        fireEvent.change(emailInput, { target: { value: 'test@example11.com' } });
+        fireEvent.change(passwordInput, { target: { value: 'testpassword' } });
 
 
-    const emailInput = screen.getByLabelText('Email:');
-    const passwordInput = screen.getByLabelText('Password:');
-    expect(emailInput).toBeInTheDocument();
-    expect(passwordInput).toBeInTheDocument();
+        fireEvent.click(screen.getByText("Login"));
+        expect(window.location.pathname).toBe('/');
+        afterEach(() => {
+            fireEvent.change(emailInput, { target: { value: '1' } });
+            fireEvent.change(passwordInput, { target: { value: '1' } });
+        });
+    })
+
+})
+
+describe("Login error handler", () => {
 
 
-    fireEvent.change(emailInput, { target: { value: 'test@example.com' } });
-    fireEvent.change(passwordInput, { target: { value: 'testPassword' } });
+    let renderComponent
+    let emailInput;
+    let passwordInput;
+    beforeEach(() => {
+        renderComponent = render(
+            <BrowserRouter>
+                <UserProvider>
+                    <Login />
+                </UserProvider>
+            </BrowserRouter>
+        )
+        emailInput = screen.getByLabelText('Email:');
+        passwordInput = screen.getByLabelText('Password:');
+
+    })
+
+    test('empty pass missing', async () => {
+        const emailInput = screen.getByLabelText(/email/i);
+        const passwordInput = screen.getByLabelText("Password:")
+        // const repeatPasswordInput = screen.getByLabelText(/confirm-password/i);
+        fireEvent.change(emailInput, { target: { value: 'test@example3.com' } });
+        fireEvent.change(passwordInput, { target: { value: '' } });
+        // fireEvent.change(repeatPasswordInput, { target: { value: 'testpassword' } });
+
+        fireEvent.click(screen.getByText("Login"));
+        await screen.findByText('Some fields is empty');
+
+        expect(screen.getByText('Some fields is empty')).toBeInTheDocument();
+
+        afterEach(() => {
+            fireEvent.change(emailInput, { target: { value: '1' } });
+            fireEvent.change(passwordInput, { target: { value: '1' } });
+        });
+
+    })
+    test('empty pass missing', async () => {
+
+        fireEvent.change(emailInput, { target: { value: '' } });
+        fireEvent.change(passwordInput, { target: { value: 'password' } });
+        fireEvent.click(screen.getByText("Login"));
+        await screen.findByText('Some fields is empty');
+
+        expect(screen.getByText('Some fields is empty')).toBeInTheDocument();
+        afterEach(() => {
+            fireEvent.change(emailInput, { target: { value: '1' } });
+            fireEvent.change(passwordInput, { target: { value: '1' } });
+        });
+
+    })
+    test('empty all missing', async () => {
+
+        fireEvent.change(emailInput, { target: { value: '' } });
+        fireEvent.change(passwordInput, { target: { value: '' } });
+        fireEvent.click(screen.getByText("Login"));
+        await screen.findByText('Some fields is empty');
+
+        expect(screen.getByText('Some fields is empty')).toBeInTheDocument();
+        afterEach(() => {
+            fireEvent.change(emailInput, { target: { value: '1' } });
+            fireEvent.change(passwordInput, { target: { value: '1' } });
+        });
+
+    })
+
+    test('Minimum characters is 5', async () => {
+
+        fireEvent.change(emailInput, { target: { value: 'test@example.com' } });
+        fireEvent.change(passwordInput, { target: { value: 'vcc' } });
 
 
-    expect(emailInput.value).toBe('test@example.com');
-    expect(passwordInput.value).toBe('testPassword');
+        fireEvent.click(screen.getByText("Login"));
+        await screen.findByText('Minimum characters is 5');
+
+        expect(screen.getByText('Minimum characters is 5')).toBeInTheDocument();
+
+        afterEach(() => {
+            fireEvent.change(emailInput, { target: { value: '1' } });
+            fireEvent.change(passwordInput, { target: { value: '1' } });
+        });
+      
+    })
+    test('Minimum characters is 9', async () => {
+        fireEvent.change(emailInput, { target: { value: 'c@abv.bg' } });
+        fireEvent.change(passwordInput, { target: { value: 'password' } });
+        fireEvent.click(screen.getByText("Login"));
+        await screen.findByText('Minimum characters is 9');
+
+        expect(screen.getByText('Minimum characters is 9')).toBeInTheDocument();
+        afterEach(() => {
+            fireEvent.change(emailInput, { target: { value: '1' } });
+            fireEvent.change(passwordInput, { target: { value: '1' } });
+        });
+    })
 
 
-    const submitButton = screen.getByText('Login');
-    fireEvent.click(submitButton);
+    afterEach(() => {
+        fireEvent.change(emailInput, { target: { value: '' } });
+        fireEvent.change(passwordInput, { target: { value: '' } });
+      });
+})
 
-    // Добавете допълнителни проверки, ако е необходимо
-});
-test('successful login redirects to home', async () => {
-
-    const successfulLogin = () => Promise.resolve();
-
-
-    render(
-        <BrowserRouter>
-            <UserProvider>
-                <Login />
-            </UserProvider>
-        </BrowserRouter>
-    );
-
-
-    const emailInput = screen.getByLabelText(/email/i);
-    const passwordInput = screen.getByLabelText("Password:")
-
-    fireEvent.change(emailInput, { target: { value: 'test@example.com' } });
-    fireEvent.change(passwordInput, { target: { value: 'testpassword' } });
-
-
-    fireEvent.click(screen.getByText("Login"));
-
-
-
-    expect(window.location.pathname).toBe('/'); 
-});
-test('login errors', async () => {
-
-    const successfulLogin = () => Promise.resolve();
-
-
-    render(
-        <BrowserRouter>
-            <UserProvider>
-                <Login />
-            </UserProvider>
-        </BrowserRouter>
-    );
-
-
-    const emailInput = screen.getByLabelText(/email/i);
-    const passwordInput = screen.getByLabelText("Password:")
-    // const repeatPasswordInput = screen.getByLabelText(/confirm-password/i);
-    fireEvent.change(emailInput, { target: { value: 'test@example.com' } });
-    fireEvent.change(passwordInput, { target: { value: '' } });
-    // fireEvent.change(repeatPasswordInput, { target: { value: 'testpassword' } });
-
-    fireEvent.click(screen.getByText("Login"));
-    await screen.findByText('Some fields is empty');
-
-
-    expect(screen.getByText('Some fields is empty')).toBeInTheDocument();
-
-    fireEvent.change(emailInput, { target: { value: 'test@example.com' } });
-    fireEvent.change(passwordInput, { target: { value: 'vcc' } });
-    // fireEvent.change(repeatPasswordInput, { target: { value: 'testpassword' } });
-
-    fireEvent.click(screen.getByText("Login"));
-    await screen.findByText('Minimum characters is 5');
-
-    expect(screen.getByText('Minimum characters is 5')).toBeInTheDocument();
-    fireEvent.change(emailInput, { target: { value: '' } });
-    fireEvent.change(passwordInput, { target: { value: 'Password' } });
-    // fireEvent.change(repeatPasswordInput, { target: { value: 'testpassword' } });
-
-    fireEvent.click(screen.getByText("Login"));
-    await screen.findByText('Some fields is empty');
-
-
-    expect(screen.getByText('Some fields is empty')).toBeInTheDocument();
-    fireEvent.change(emailInput, { target: { value: 'test@ex' } });
-    fireEvent.change(passwordInput, { target: { value: 'Password' } });
-    // fireEvent.change(repeatPasswordInput, { target: { value: 'testpassword' } });
-
-    fireEvent.click(screen.getByText("Login"));
-    await screen.findByText('Minimum characters is 9');
-
-
-    expect(screen.getByText('Minimum characters is 9')).toBeInTheDocument();
-
-});
