1dc7faaf311b404ef3bad791ddf908c184b0531e
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-12-01 12:26:40 +0200

    add

--- soft-academy/src/components/CatalogCourses/CatalogCourses.jsx
+++ soft-academy/src/components/CatalogCourses/CatalogCourses.jsx
@@ -1,4 +1,5 @@
 import { useContext, useState, useEffect } from "react"
+
 import './bookMark.css'
 import { Bounce, Fade, Flip, Hinge, JackInTheBox, Roll, Rotate, Slide, Zoom } from "react-awesome-reveal";
 
@@ -10,9 +11,10 @@ import Footer from "../Footer/Footer"
 import { usePaginations } from "../Hooks/usePaginations";
 import { Pagination } from "../Pagination/Pagination";
 import { useLocation } from "react-router-dom";
+import { AdminSidebar } from "../AdminDashboard/AdminSideBar";
 
 const CatalogCourses = () => {
- 
+
 
     const { courses } = useContext(CourseContext)
     const [isLoading, setIsLoading] = useState(true)
@@ -23,7 +25,7 @@ const CatalogCourses = () => {
 
     const { getPaginationData } = usePaginations(resultsPerPage)
 
-    const {totalPages,currentPage,currentResult,paginate,setCurrentPage}= getPaginationData(coursesInfo)
+    const { totalPages, currentPage, currentResult, paginate, setCurrentPage } = getPaginationData(coursesInfo)
 
     // const handleBack = () => {
     //     if (currentPage > 1) {
@@ -43,14 +45,19 @@ const CatalogCourses = () => {
 
     return (
         <>
+
             {isLoading ? <IsLoading /> : (<> <Fade delay="10" duration="2000" triggerOnce='true'><section id="explore" className="explore">
 
-
+           
                 <div className="container">
+                <div className="image-section">
+                        {/* Добавете снимката тук */}
+                        <img src="https://thumbs.dreamstime.com/b/foss-word-cloud-concept-vector-illustration-170695446.jpg" alt="Course Catalog Image" />
+                    </div>
                     <div className="section-header">
-                        <h2 style={{color: "rgb(0 156 233)"}}>Your Gateway to Diverse Courses</h2>
-                        <p style={{color:"white", fontSize:"22px"}}>Discover a comprehensive catalog of courses covering a myriad of subjects. 
-                        From technology and business to arts and sciences, explore courses curated to enhance your skills and knowledge. Your educational journey starts here explore, learn, and excel with our diverse course catalog.</p>
+                        <h2 style={{ color: "rgb(0 156 233)" }}>Your Gateway to Diverse Courses</h2>
+                        <p style={{ color: "white", fontSize: "22px" }}>Discover a comprehensive catalog of courses covering a myriad of subjects.
+                            From technology and business to arts and sciences, explore courses curated to enhance your skills and knowledge. Your educational journey starts here explore, learn, and excel with our diverse course catalog.</p>
                     </div>
 
                     <div className="explore-content">
