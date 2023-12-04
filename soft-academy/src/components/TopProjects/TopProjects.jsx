87350cb0d14c31bc17ed7fbc9d90e9ed33ec7c27
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-12-04 10:47:34 +0200

    add second part of Top Projects

--- soft-academy/src/components/TopProjects/TopProjects.jsx
+++ soft-academy/src/components/TopProjects/TopProjects.jsx
@@ -6,14 +6,16 @@ export const TopProjects=()=>{
 return(
     <>
        <section className="section-a">
-      <div className="title-projects">Section A Title</div>
+      <div className="title-projects"><h2>Educational Ambitions: Selected Top Student Projects Demonstrating the Power of Learning and Development</h2></div>
       <div className="data-projects">
-        <div className="rights-projects">Rights</div>
-        <div className="description-projects">Description</div>
+        <div className="rights-projects">Dec 10, 2023</div>
+        <div className="separator"></div>
+        <div className="description-projects">Discover the brilliance of 'Educational Ambitions' through selected student projects. Unveil the impact of learning and development in innovative endeavors. From groundbreaking technologies to creative solutions, witness the power of ambitious students shaping the future of education.</div>
       </div>
       <div className="youtube-preview">
-      <iframe width="996" height="729" src="https://www.youtube.com/embed/Nn1IeHL5DHY" title="Ricchi E Poveri - Sarà perché ti amo (Thommys Popshow extra 17.12.1983)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <iframe width="1080" height="520" src="https://www.youtube.com/embed/duP_AI2B2XM" title="Solution Challenge Demo Day 2023" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       </div>
+      <div className="title-projects"><h3>Educational Ambitions: Selected Top Student Projects Demonstrating the Power of Learning and Development</h3></div>
     </section>
     </>
 )
